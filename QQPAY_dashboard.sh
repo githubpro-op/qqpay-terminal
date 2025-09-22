e383dc72aec8d81982abf74abf7b82e42ea07a1b
githubpro-op <70138426+githubpro-op@users.noreply.github.com>
2025-09-22 12:19:06 +0530

    Update QQPAY_dashboard.sh

--- QQPAY_dashboard.sh
+++ QQPAY_dashboard.sh
@@ -114,7 +114,7 @@ cat <<'EOF' | sudo tee resources/views/dashboard.blade.php
 @include('sections.leaderboard-modal')
 @include('sections.rewards-modal')
 @include('sections.refer-modal')
-@include('sections.daily-modal')
+@include('sections.dailyearn-modal')
   <!-- Sections -->
   <div id="home" class="section active">
     <h2> </h2>
